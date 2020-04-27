5a670c52058002124cc713f1d2ab0dc47105fbb8
Stanisław Margielewski <stanislaw.margielewski@gmail.com>
2020-04-27 23:09:04 +0200

    title

--- src/sections/ProjectTitle.js
+++ src/sections/ProjectTitle.js
@@ -3,16 +3,9 @@ import { Link, } from "react-scroll";
 
 class ProjectTitle extends Component {
     render() {
-        const title = this.props.title
+
         return (
-            <Link className='navBottom__link'
-                activeClass="active"
-                to="project"
-                spy={true}
-                smooth={true}
-                offset={-100}
-                duration={500}
-            >{title}</Link>
+           null
         )
     }
 }
