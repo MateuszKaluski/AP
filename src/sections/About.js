a8ddcbde19da2f91ec7eebec4dd0feef763cbccf
Stanisław Margielewski <stanislaw.margielewski@gmail.com>
2020-04-29 15:37:04 +0200

    about

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,12 +1,11 @@
-import React, {Fragment} from "react";
-import {Nav} from "../components/Nav";
+import React, { Fragment } from "react";
+import { Nav } from "../components/Nav";
 
-import HeaderSubpage from './HeaderSubpage';
-import NavSubpages from '../components/NavSubpages';
+
 
 const About = () => (
     <Fragment>
-        <Nav src="about" color={'rgba(206, 45, 36, 1)'}/>
+        <Nav src="about" color={'rgba(206, 45, 36, 1)'} />
         <div className="page">
             <section className='about' id='about'>
                 <div className='about__container'>
@@ -15,20 +14,20 @@ const About = () => (
                         <img
                             className='foto-size'
                             alt={'autor'}
-                            src={require('../images/autor/2.jpg')}/>
+                            src={require('../images/autor/2.jpg')} />
                     </div>
                     <div className='about__info'>
                         <h1 className='about__autor'>__Cześć</h1>
                         <p className='about__textLight'>Jestem projektantką. Kreowanie jest dla mnie
-                            najlepszą formą spędzania czasu. Uwielbiam poszerzać swoją wiedzę, dlatego każdy
-                            projekt, z różnych dziedzin jest dla mnie wyzwaniem i pasją.
+                        najlepszą formą spędzania czasu. Uwielbiam poszerzać swoją wiedzę, dlatego każdy
+                        projekt, z różnych dziedzin jest dla mnie wyzwaniem i pasją.
                         </p>
                         <p className='about__textBold'>Projektuję:
                         </p>
                         <p className='about__textBold'>strony internetowe__ materiały do social media__
-                            materiały graficzne do internetu__ materiały promocyjne__ materiały POS__
-                            szablony prezentacji__ plakaty__ etykiety__ opakowania__ gadżety__ logo__
-                            animacje__ koncepcje kreatywne
+                        materiały graficzne do internetu__ materiały promocyjne__ materiały POS__
+                        szablony prezentacji__ plakaty__ etykiety__ opakowania__ gadżety__ logo__
+                        animacje__ koncepcje kreatywne
                         </p>
                         <p className='about__textLight'>Masz jakiś niekonwencjonalny projekt? Napisz!
                         </p>
